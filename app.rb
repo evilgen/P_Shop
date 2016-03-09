9f8de489c38856ea31fdccd2125af398c20dca23
Kovalev Evgeniy <e.evilgen@gmail.com>
2016-03-09 21:37:20 +0300

    Add rakefile, update gemfile, add model "Product"

--- app.rb
+++ app.rb
@@ -2,6 +2,14 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sinatra/activerecord'
+
+#Создание подключения к БД при помощи Active Record
+set :database, "sqlite3:pizzashop.db"
+
+class Produt < ActiveRecord::Base
+end
+
 
 get '/' do
 	erb :index
