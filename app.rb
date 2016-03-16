c6a20b28c98706937f6f4c35181496219a11591e
Kovalev Evgeniy <e.evilgen@gmail.com>
2016-03-16 22:34:28 +0300

    Save order & cutomer information

--- app.rb
+++ app.rb
@@ -10,6 +10,9 @@ set :database, "sqlite3:pizzashop.db"
 class Product < ActiveRecord::Base
 end
 
+class Order < ActiveRecord::Base
+end
+
 
 get '/' do
 	@products = Product.all
@@ -34,4 +37,10 @@ end
 def parse_order_line orders_input
 	arr = (orders_input.delete "product_").split(',').map{|el| el.split('=')}
 	return arr
+end
+
+post '/place_order' do
+  order = Order.new params[:order]
+  order.save
+  erb "Ваш заказ принят."
 end
